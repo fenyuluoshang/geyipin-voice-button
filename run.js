cc22d4f262df2761c1c6e677c4575f782560e9a8
Fen Yu <gzj0078@icloud.com>
2024-02-05 16:55:32 +0800

    Fix: show frontend errors

--- run.js
+++ run.js
@@ -3,8 +3,8 @@ const child_process = require('child_process');
 const frontend = child_process.exec('cd frontend && pnpm serve')
 const backend = child_process.exec('cd server && pnpm start')
 
-backend.stderr.on('data', (data)=>{
-  console.error(`[backend]: ${data.toString('utf8')}`)
+frontend.stderr.on('data', (data)=>{
+  console.error(`[frontend]: ${data.toString('utf8')}`)
 })
 frontend.stdout.on('data', (data)=>{
   console.log(`[frontend]: ${data.toString('utf8')}`)
