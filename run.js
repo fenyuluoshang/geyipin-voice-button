b3d350b0180bbf4a15a703a0db58638d2f777bd6
Fen Yu <gzj0078@icloud.com>
2024-02-05 16:09:45 +0800

    Fix: add error listener

--- run.js
+++ run.js
@@ -2,10 +2,17 @@ const child_process = require('child_process');
 
 const frontend = child_process.exec('cd frontend && pnpm serve')
 const backend = child_process.exec('cd server && pnpm start')
+
+backend.stderr.on('data', (data)=>{
+  console.error(`[backend]: ${data.toString('utf8')}`)
+})
 frontend.stdout.on('data', (data)=>{
   console.log(`[frontend]: ${data.toString('utf8')}`)
 })
 
+backend.stderr.on('data', (data)=>{
+  console.error(`[backend]: ${data.toString('utf8')}`)
+})
 backend.stdout.on('data', (data)=>{
   console.log(`[backend]: ${data.toString('utf8')}`)
 })
